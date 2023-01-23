f762d32998271a6e352219bc1ed1b1ed6ce09c49
Damien <damsmailspro@gmail.com>
2023-01-23 15:47:36 +0100

    maj

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Loader from '../components/Loader/loader';
 import { useState, useEffect } from 'react';
-import { Quicksand } from '@next/font/google';
+import { Quicksand, Berkshire_Swash } from '@next/font/google';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import { store } from '../redux/store';
 import { Provider } from 'react-redux';
@@ -10,6 +10,10 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const quicksand = Quicksand({ subsets: ["latin"] });
+const berkshireSwash = Berkshire_Swash({
+  weight: '400',
+  subsets: ['latin']
+})
 
 const queryClient = new QueryClient();
 
@@ -33,7 +37,7 @@ export default function App({ Component, pageProps }: AppProps) {
         loading ? <Loader message="Chargement..." /> : (
           <>
             <style jsx global>
-              {`html {font-family: ${quicksand.style.fontFamily};}`}
+              {`html {font-family: ${quicksand.style.fontFamily};}, .northgarden {font-family: ${berkshireSwash.style.fontFamily}}`}
             </style>
             <Provider store={store}>
               <Component {...pageProps} />
